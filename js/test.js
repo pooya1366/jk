c1649de67bd177cd9acfc94a8c20e64c021a3f27
Pooya Saberian <pooya.saberian@gmail.com>
2013-11-06 01:17:41 +0330

    jk

---
+++ js/test.js
@@ -0,0 +1,54 @@
+carousel = function(btn, e){
+    e.preventDefault();
+    if(!$(btn).hasClass('disabled')){
+
+        parent_carousel.children('.nav').unbind();
+
+        var parent_carousel = $(btn).parent('.carousel');
+        var scroll_val = parent_carousel.children('.items-container').width();
+        var margin_right = parseInt(parent_carousel.
+            find('ul.items').
+            css('margin-right'));
+        var count_items = parent_carousel.find('li.item').length;
+        var item_width = parent_carousel.find('li.item').width();
+        var items_in_row = scroll_val / item_width;
+        var speed = items_in_row * 100;
+        var min_margin_right = (parseInt((count_items*item_width) / scroll_val)*scroll_val) * -1;
+        var max_margin_right = 0;
+
+        if($(btn).hasClass('next')) {
+            var calculated_margin_right = margin_right - scroll_val;
+        }
+        else if($(btn).hasClass('previous')) {
+            var calculated_margin_right = margin_right + scroll_val;
+        }
+
+        parent_carousel.find('.items').animate({
+            'marginRight': calculated_margin_right
+        }, speed, function(){
+            var margin_right = parseInt(parent_carousel.find('.items').css('margin-right'));
+            if(margin_right == max_margin_right) {
+                parent_carousel.children('.nav.previous').addClass('disabled');
+                parent_carousel.children('.nav.next').removeClass('disabled');
+            }
+            else if(margin_right == min_margin_right) {
+                parent_carousel.children('.nav.next').addClass('disabled');
+                parent_carousel.children('.nav.previous').removeClass('disabled');
+            }
+            else {
+                parent_carousel.children('.nav.next').removeClass('disabled');
+                parent_carousel.children('.nav.previous').removeClass('disabled');
+            }
+            parent_carousel.children('.nav').bind();
+        });
+    }
+
+}
+$('.carousel .nav').click(function(e){
+    carousel(this, e);
+});
+$(window).resize(function() {
+    $('.carousel .items').css('margin-right', 0);
+    $('.carousel .nav.next').removeClass('disabled');
+    $('.carousel .nav.previous').addClass('disabled');
+});
