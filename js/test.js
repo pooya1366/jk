38317f8a274ea8ff1738677c43eeea44d5729d95
Iman Mohamadi <turbosianor@gmail.com>
2013-11-08 00:55:24 +0330

    A jQuery plugin was made based on js/test.js

--- js/test.js
+++ js/test.js
@@ -1,40 +1,47 @@
 get_carousel_vars = function(carousel) {
 
+    //better to be changed to $node
     var parent_carousel = carousel;
 
+    //width of main div of carousel, how much should be scrolled
+    //better to be changed to carouselWidth
     var scroll_val = parent_carousel.children('.items-container').width();
-    var margin_right = parseInt(parent_carousel.find('ul.items').css('margin-right'));
+    var margin_right = parseInt( parent_carousel.find('ul.items').css('margin-right') );
     var items = parent_carousel.find('li.item');
+    //better to be changed to itemsCount
     var count_items = items.length;
+    //better to be changed to itemWidth
     var item_width = items.width();
+    //better to be change to item per row
     var items_in_row = scroll_val / item_width;
     var speed = items_in_row * 80;
+    //better to be changed to pagesCount
     var num_of_pages = Math.ceil((count_items*item_width)/scroll_val)
     var min_margin_right = (num_of_pages - 1) * scroll_val * -1;
     var max_margin_right = 0;
 
-    var return_vars = new Array();
+    var result = {};
 
-    return_vars['scroll_val'] = scroll_val;
-    return_vars['margin_right'] = margin_right;
-    return_vars['count_items'] = count_items;
-    return_vars['items'] = items;
-    return_vars['item_width'] = item_width;
-    return_vars['items_in_row'] = items_in_row;
-    return_vars['speed'] = speed;
-    return_vars['num_of_pages'] = num_of_pages;
-    return_vars['min_margin_right'] = min_margin_right;
-    return_vars['max_margin_right'] = max_margin_right;
+    result.scroll_val = scroll_val;
+    result.margin_right = margin_right;
+    result.count_items = count_items;
+    result.items = items;
+    result.item_width = item_width;
+    result.items_in_row = items_in_row;
+    result.speed = speed;
+    result.num_of_pages = num_of_pages;
+    result.min_margin_right = min_margin_right;
+    result.max_margin_right = max_margin_right;
 
-    return return_vars;
-
+    return result;
 }
+
 initialize_carousel = function(carousel) {
 
     var carousel_vars = get_carousel_vars(carousel);
-    var count_items = carousel_vars['count_items'];
-    var items_in_row = carousel_vars['items_in_row'];
-    var items = carousel_vars['items'];
+    var count_items = carousel_vars.count_items;
+    var items_in_row = carousel_vars.items_in_row;
+    var items = carousel_vars.items;
 
     carousel.children('.items-container').children('ul.items').css('margin-right', 0);
     carousel.children('.nav.next').removeClass('disabled');
@@ -108,3 +115,4 @@ $(document).ready(function() {
         initialize_carousel($(this));
     });
 });
+
