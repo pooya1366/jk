f5940aca31c783fbd8f2c4bd9314b80121074e85
Iman Mohamadi <turbosianor@gmail.com>
2013-11-28 12:16:35 +0330

    products page migrated to require js

---
+++ wrapper/js/src/events/categoryViewToggle.js
@@ -0,0 +1,8 @@
+define(['jquery', 'js/handlers/categoryViewToggle'], function() {
+    return (function () {
+        $(document).ready(function () {
+            jk.handlers.categoryViewToggleInstaller();
+        });
+    })();
+
+});
