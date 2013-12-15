2c8d83a55c0c52b28e5df88d820e0acea596eb0f
Iman Mohamadi <Iman@ImanMCBKA.local>
2013-12-16 01:37:42 +0330

    bug fixes

--- js/src/events/slidingMenu.common.js
+++ js/src/events/slidingMenu.common.js
@@ -4,13 +4,13 @@ $('body').delegate('.list-dropdown', 'mouseenter', function (e) {
     //close all other dropdown-menus
     jk.handlers.closeAllDropDowns();
     //hold for 100ms then open the menu
-    clearTimeout(jkTimeOuts.openSlideMenu);
     jkTimeOuts.openSlideMenu = setTimeout(function () {
         jk.handlers.openDropdownMenu(e.target);
-    },100);
+    },200);
 });
 
 $('body').delegate('.list-dropdown', 'mouseleave', function (e) {
+    clearTimeout(jkTimeOuts.openSlideMenu);
     jkTimeOuts.closeSlideMenu = setTimeout(function() {
         jk.handlers.closeDropdownMenu(e.target);
     }, 300);
