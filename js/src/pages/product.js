f42ae0c3f23daeed1ec3851f1fbae7ef4346438b
Iman Mohamadi <Iman@ImanMCBKA.local>
2013-12-17 19:42:49 +0330

    More styling on fancyBox
    image paths corrected, more room added to the bottom of the white box,
    and openning transition changed

--- js/src/pages/product.js
+++ js/src/pages/product.js
@@ -69,6 +69,10 @@ require(['jquery',
 
     $(document).ready(function () {
         $('ul.fancy-box a').fancybox( {
+            openEffect: 'elastic',
+            closeEffect: 'none',
+            arrows: true,
+            margin: [40, 40, 60, 40],
             helpers: {
                 thumbs: {
                     width: 50,
