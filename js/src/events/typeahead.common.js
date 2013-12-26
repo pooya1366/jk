fff1e3335a96fcf6fe458a531cdee4b2c6b60a8b
Iman Mohamadi <Iman@ImanMCBKA.local>
2013-12-27 00:03:27 +0330

    typeahead groups added

--- js/src/events/typeahead.common.js
+++ js/src/events/typeahead.common.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-    $('#search-text').typeahead({
-        name: 'products',
-        local: ['book', 'booklet', 'book case', 'trap book', 'green book', 'phone book']
-    });
+    $('#search-text').typeahead([
+        {
+            name: 'nba-teams',
+            local: ['book', 'booking', 'book store'],
+            header: '<h3 class="league-name">NBA Teams</h3>'
+        },
+        {
+            name: 'nhl-teams',
+            local: ['booker', 'book keeper', 'book keeping'],
+            header: '<h3 class="league-name">NHL Teams</h3>'
+        }
+    ]);
 });
