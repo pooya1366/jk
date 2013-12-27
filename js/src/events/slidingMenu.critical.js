bff73962a3bc45959fce998e4a91d3979eaeab13
Iman Mohamadi <Iman@ImanMCBKA.local>
2013-12-27 19:58:11 +0330

    sliding menu console logse removed

--- js/src/events/slidingMenu.critical.js
+++ js/src/events/slidingMenu.critical.js
@@ -1,8 +1,6 @@
 (function () {
-    console.log('working...');
     window.jkTimeOuts = window.jkTimeOuts || {};
     $('html').delegate('.list-dropdown', 'mouseenter', function (e) {
-        console.log('operating');
         //hold for 100ms then open the menu
         clearTimeout(jkTimeOuts.openSlideMenu);
         jkTimeOuts.openSlideMenu = setTimeout(function () {
@@ -14,7 +12,6 @@
     });
 
     $('html').delegate('.list-dropdown', 'mouseleave', function (e) {
-        console.log('operating');
         clearTimeout(jkTimeOuts.openSlideMenu);
         jkTimeOuts.closeSlideMenu = setTimeout(function() {
             jk.handlers.closeDropdownMenu(e.target);
