ec3e1d177d2995ca503879e82ecdcec9f06a1af0
Iman Mohamadi <Iman@ImanMCBKA.local>
2013-12-31 14:17:30 +0330

    more options added to slideShow installer

--- js/src/handlers/slideShow.common.js
+++ js/src/handlers/slideShow.common.js
@@ -9,11 +9,17 @@
             options = {
                 reverse: true,
                 animation: 'slide',
-                direction: 'horizontal'
+                direction: 'horizontal',
+                controlNav: false,
+                directionNav: false,
+                slideshowSpeed: 70000
             };
 
         options.animation = $node.attr('data-animation') || options.animation;
         options.direction = $node.attr('data-direction') || options.direction;
+        options.directionNav = $node.attr('data-arrows') || options.directionNav;
+        options.controlNav = $node.attr('data-pages') || options.controlNav;
+        options.slideshowSpeed = $node.attr('data-speed') || options.slideshowSpeed;
 
         $node.flexslider(options);
 
