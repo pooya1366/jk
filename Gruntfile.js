528a44edb6309dcb2a108f14c4c54ecd22f216b7
Iman Mohamadi <Iman@ImanMCBKA.local>
2014-02-14 17:10:07 +0330

    uglifications added

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
             },
             vents: {
                 options: {
-                    banner: "define(['jquery', 'commonPlugins'], function () {",
+                    banner: "define(['commonPlugins'], function () {",
                     footer: '});'
                 },
                 files : [
@@ -24,10 +24,14 @@ module.exports = function (grunt) {
                 ]
             },
             plugins: {
+                options: {
+                    banner: "define(function () { return (function() {",
+                    footer: '})() });'
+                },
                 src: [
                         'js/src/plugins/*.common.js',
                         'js/libs/typeahead.js/dist/typeahead.js'
-                     ],
+                    ],
                 dest: 'js/dist/plugins/commonPlugins.js'
             },
             fancyBox: {
@@ -40,7 +44,8 @@ module.exports = function (grunt) {
             bootstrap: {
                 src: [
                     'js/libs/bootstrap/js/transition.js',
-                    'js/libs/bootstrap/js/modal.js'
+                    'js/libs/bootstrap/js/modal.js',
+                    'js/libs/bootstrap/js/dropdown.js'
                 ],
                 dest: 'js/dist/libs/bootstrap.js',
                 nonull: true
@@ -48,12 +53,12 @@ module.exports = function (grunt) {
             critical: {
                 src: [
                     'js/libs/jquery/jquery.js',
+                    'js/libs/typeahead.js/dist/typeahead.js',
+                    'js/libs/typeahead.js/test/vendor/hogan-2.0.0.js',
                     'js/src/extras/noConflict.js',
                     'js/src/plugins/*.critical.js',
                     'js/src/handlers/*.critical.js',
-                    'js/src/events/*.critical.js',
-                    'js/libs/typeahead.js/test/vendor/hogan-2.0.0.js',
-                    'js/libs/typeahead.js/dist/typeahead.min.js'
+                    'js/src/events/*.critical.js'
                 ],
                 dest: 'js/dist/critical.js',
                 nonull: true
@@ -61,13 +66,65 @@ module.exports = function (grunt) {
         },
 
         uglify: {
-            bootstrap : {
-                src : 'js/dist/libs/bootstrap.js',
-                dest : 'js/dist/libs/bootstrap.min.js'
-            },
             critical : {
-                src : 'js/dist/critical.js',
+                src : '<%= concat.critical.dest %>',
                 dest : 'js/dist/critical.min.js'
+            },
+            fancyBox: {
+                src: '<%= concat.fancyBox.dest %>',
+                dest: 'js/dist/plugins/fancyBox.min.js'
+            },
+            commonPlugins: {
+                src: '<%= concat.plugins.dest %>',
+                dest: 'js/dist/plugins/commonPlugins.min.js'
+            },
+            commonVents: {
+                src: 'js/dist/vents/commonVents.js',
+                dest: 'js/dist/vents/commonVents.min.js'
+            },
+            compare: {
+                src: 'js/dist/vents/compare.js',
+                dest: 'js/dist/vents/compare.min.js'
+            },
+            events: {
+                files: {
+                    'js/dist/events/affixEvent.min.js': 'js/src/events/affixEvent.js',
+                    'js/dist/events/captionHeader.min.js': 'js/src/events/captionHeader.js',
+                    'js/dist/events/categoryViewToggle.min.js': 'js/src/events/categoryViewToggle.js',
+                    'js/dist/events/compare.min.js': 'js/src/events/compare.js',
+                    'js/dist/events/modals.min.js': 'js/src/events/modals.js',
+                    'js/dist/events/navScroller.min.js': 'js/src/events/navScroller.js',
+                    'js/dist/events/scrollSpy.min.js': 'js/src/events/scrollSpy.js'
+                }
+            },
+            handlers: {
+                files: {
+                    'js/dist/handlers/affixEvent.min.js': 'js/src/handlers/affixHandler.js',
+                    'js/dist/handlers/captionHeader.min.js': 'js/src/handlers/captionHeader.js',
+                    'js/dist/handlers/categoryViewToggle.min.js': 'js/src/handlers/categoryViewToggle.js',
+                    'js/dist/handlers/compare.min.js': 'js/src/handlers/compare.js',
+                    'js/dist/handlers/compareButton.min.js': 'js/src/handlers/compareButton.js',
+                    'js/dist/handlers/scrollSpyHandler.min.js': 'js/src/handlers/scrollSpyHandler.js',
+                    'js/dist/handlers/fancyBoxHandler.min.js': 'js/src/handlers/fancyBoxHandler.js'
+                }
+            },
+            plugins: {
+                files: {
+                    'js/dist/plugins/affix.min.js': 'js/src/plugins/affix.js',
+                    'js/dist/plugins/jQuery.jkNotification.min.js': 'js/src/plugins/jQuery.jkNotification.js',
+                    'js/dist/plugins/scrollspy.min.js': 'js/src/plugins/affix.js',
+                    'js/dist/plugins/stickySidebar.min.js': 'js/src/plugins/stickySidebar.js'
+                }
+            },
+            libs: {
+                files: {
+                    'js/dist/libs/bootstrap.min.js': 'js/dist/libs/bootstrap.js',
+                    'js/dist/libs/html5shiv.min.js': 'js/libs/html5shiv/dist/html5shiv.js',
+                    'js/dist/libs/jquery.cookie.min.js': 'js/libs/jqueryCookie/jquery.cookie.js',
+                    'js/dist/libs/modernizr.min.js': 'js/libs/modernizr/modernizr.js',
+                    'js/dist/libs/requirejs.min.js': 'js/libs/requirejs/require.js',
+                    'js/dist/plugins/scrollspy.min.js': 'js/libs/bootstrap/js/scrollspy.js'
+                }
             }
         },
 
@@ -78,13 +135,15 @@ module.exports = function (grunt) {
                 },
                 files: [
                     { expand: true, cwd: 'less/jk', src: '**/*-page.less', dest: 'css/', ext: '.css' },
-                    {src: ['less/jk/jk.less',
+                    {
+                        src: ['less/jk/jk.less',
                             'js/libs/fancyBox/source/jquery.fancybox.css',
                             'js/libs/flexSlider/flexslider.css',
                             'js/libs/fancyBox/source/helpers/jquery.fancybox-thumbs.css',
                             'less/overwrites/*.less'
                           ],
-                        dest: 'css/styles.css'}
+                        dest: 'css/styles.css'
+                    }
                 ]
             }
         },
@@ -148,6 +207,20 @@ module.exports = function (grunt) {
   	grunt.registerTask('makePlugins', ['concat:plugins']);
   	grunt.registerTask('watchTheme', ['watch:themes']);
   	grunt.registerTask('watchVents', ['watch:vents']);
-  	grunt.registerTask('bootstrap', ['concat:bootstrap', 'uglify:bootstrap']);
 
+    grunt.registerTask('release', [
+        'concat:vents',
+        'concat:plugins',
+        'concat:critical',
+        'less:production',
+        'uglify:critical',
+        'uglify:fancyBox',
+        'uglify:commonPlugins',
+        'uglify:commonVents',
+        'uglify:compare',
+        'uglify:events',
+        'uglify:handlers',
+        'uglify:plugins',
+        'uglify:libs'
+    ]);
 }
